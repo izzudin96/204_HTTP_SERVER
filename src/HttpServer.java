64f1dbc2ef9203ba64248a873edcf9b2b3325386
Izzudin Anuar <izzudinanuar96@gmail.com>
2018-08-04 02:37:47 +1200

    at least have some progress.

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -2,74 +2,76 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-
-class HttpServer
-{
-    public static void main(String args[]) throws IOException, ClassNotFoundException
-    {
-        int portNumber = 8080;
-
-        System.out.println("web server is starting...");
-
-        ServerSocket server = new ServerSocket(portNumber);
-        print("Server is running on port: " + server.getLocalPort());
-
-        while(true) {
-            try {
-                Socket clientSocket = server.accept();
-                HttpServerSession session = new HttpServerSession(clientSocket);
-            } finally {
-                server.close();
-            }
-        }
-    }
-
-    public static void print(String message) {
-        System.out.println(message);
-    }
-}
-
 class HttpServerSession extends Thread
 {
-    private Socket client;
     private HttpServer server;
+    private Socket client;
     private PrintWriter writer;
 
-    public HttpServerSession(Socket socket) {
-        this.client = socket;
-        run();
+    public HttpServerSession(HttpServer a, Socket b) {
+        server = a;
+        client = b;
+        writer = null;
     }
 
     public void run() {
         try {
             writer = new PrintWriter(client.getOutputStream(), true);
             BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            while(true) {
-                /* when the client disconnects, readLine returns null */
+
+            System.out.println("Connection received from: " + client.getRemoteSocketAddress());
+            while(client.isConnected()) {
                 String line = reader.readLine();
-                if(line != null) {
-                    System.out.println(line);
-                    System.out.println("Connection from: " + client.getLocalAddress());
-                }
-                else {
-                    break;
-                }
+                System.out.println(line);
             }
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.err.println("Exception: " + e);
         }
 
+        server.logout(this);
+    }
+}
+
+class HttpServer
+{
+    private ArrayList<HttpServerSession> sessions;
+
+    public static void main(String args[]) {
+        HttpServer server = new HttpServer();
+        server.start_server();
+    }
+
+    public void start_server() {
+        sessions = new ArrayList<HttpServerSession>();
+
         try {
-            client.close();
-        } catch (IOException e) {
+            ServerSocket server = new ServerSocket(8080);
+            System.out.println("Starting server on port: " + server.getLocalPort());
+            while(true) {
+                Socket client = server.accept();
+                HttpServerSession thread = new HttpServerSession(this, client);
+                synchronized (sessions) {
+                    sessions.add(thread);
+                }
+                thread.start();
+            }
+        } catch (Exception e) {
             System.err.println("Exception: " + e);
         }
     }
 
-    public void send(String message)
+    public void logout(HttpServerSession session)
     {
-        if(writer != null)
-            writer.println(message);
+        int i, len;
+        synchronized(sessions) {
+            len = sessions.size();
+            for(i=0; i<len; i++) {
+                HttpServerSession s = sessions.get(i);
+                if(s == session) {
+                    sessions.remove(i);
+                    break;
+                }
+            }
+        }
     }
 }
